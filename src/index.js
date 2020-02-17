b136c10383192178ca33e64b7168c2e88911432c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-02-16 21:22:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom';
 import Game from './components/Game';
 import './style.css';
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render( <Game />, document.getElementById('root'));
