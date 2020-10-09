cd243aa7dc08db1381142d6d2564a48f8a7e01bd
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-09 12:23:26 -0400

    Done.

--- .history/src/components/Game_20201009114458.js
+++ .history/src/components/Game_20201009114458.js
@@ -17,10 +17,7 @@ class Game extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will set the current time
-  componentDidMount(){
-    this.setCurrentTime();
-  }
-
+  
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
   }
--- .history/src/components/Pancake_20201009114458.js
+++ .history/src/components/Pancake_20201009114458.js
@@ -11,15 +11,8 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
-  componentDidMount() {
-    this.startInterval();
-  }
 
-    // TODO: create a componentWillUnmount() which will clear the interval
-  componentWillUnmount() {
-    this.cleanUpInterval()
-  }
-
+  // TODO: create a componentWillUnmount() which will clear the interval
 
   updateCounter = () => {
     this.setState({
