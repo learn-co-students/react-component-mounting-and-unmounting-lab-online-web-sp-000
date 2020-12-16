2b3ca443111e4f6b99bc918eee7ca6e711438211
connorworthen <connorw220@protonmail.com>
2020-12-15 18:00:17 -0800

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -11,8 +11,13 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
-
+  componentDidMount() {
+    setInterval(this.updateCounter)
+  }
   // TODO: create a componentWillUnmount() which will clear the interval
+  componentWillUnmount() {
+    clearInterval(this.componentDidMount)
+  }
 
   updateCounter = () => {
     this.setState({
